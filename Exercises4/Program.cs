db349f4d8afff94f112ad308c90eb0ed3c57b1a7
Hien753 <scale1941@gmail.com>
2022-07-11 21:11:33 +0900

    HoT: initial revision

--- Exercises4/Program.cs
+++ Exercises4/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace Exercises4
 {
-    internal class Program
+    class Program
     {
+        static int countHeads;
+        static int countTails;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CoinToss();
+        }
+        static void CoinToss()
+        {
+            countHeads = 0;
+            countTails = 0;
+            Random random = new Random();
+            Console.WriteLine("Tossing a coin...");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Round " + (i + 1) + " ");
+                if (random.Next(0, 2) == 0)
+                {
+                    Console.WriteLine("Heads");
+                    countHeads++;
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                    countTails++;
+                }
+            }
+            Console.WriteLine("Heads: " + countHeads + ", Tails: " + countTails);
         }
     }
 }
